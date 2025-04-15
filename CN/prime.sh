1277b618625f2d9315356b08a714946927ff976e
aswin3417 <aswinac090@gmail.com>
2025-04-15 11:39:53 +0530

    Update prime.sh

--- CN/prime.sh
+++ CN/prime.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+echo "Enter a number:"
+read n
+if [ $n -lt 2 ]; then
+    echo "$n is not a prime number."
+    exit 1
+fi
 
-clear
-read -p "enter the number" n
 for (( i=2;i<=$n/2;i++ ))
 do
-	if (( $n % $i == 0 ))
-	then
-		echo "$n is not prime"
-		exit 0
-	fi
+        if (( $n % $i == 0 ))
+        then
+        echo "$n is not prime"
+        exit 0
+fi
 done
 echo "prime number"
